d744443fd89e7da4a8cc654ad12817a264c64c11
uther518 <uther518@gmail.com>
2023-03-21 00:37:51 +0800

    es

--- flink-kafka-es/src/main/java/com/medata/flink/FlinkKafkaES.java
+++ flink-kafka-es/src/main/java/com/medata/flink/FlinkKafkaES.java
@@ -17,23 +17,29 @@
  */
 package com.medata.flink;
 
-
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
+import com.medata.flink.biz.KafkaMessageTransform;
+import com.medata.flink.elasticsearch.ElasticsearchSink;
+import com.medata.flink.utils.CountTriggerWithTimeout;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlinkKafkaES {
 
@@ -41,6 +47,42 @@ public class FlinkKafkaES {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
 
+        try {
+            env.enableCheckpointing(10000);
+            //topic列表
+            String[] topics = new String[]{"mysqlbinlog_test_tb_order"};
+            KafkaSource<String> source = KafkaSource.<String>builder()
+                    .setBootstrapServers("127.0.0.1:9092")
+                    .setTopics(topics)
+                    .setGroupId("my-group")
+                    .setStartingOffsets(OffsetsInitializer.latest())
+                    .setValueOnlyDeserializer(new SimpleStringSchema())
+                    .build();
+
+            SingleOutputStreamOperator<JSONObject> kafkaStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source").map(new MapFunction<String, JSONObject>() {
+                public JSONObject map(String value) throws Exception {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject = JSON.parseObject(value);
+                    jsonObject = KafkaMessageTransform.Message(jsonObject);
+                    return jsonObject;
+                }
+            });
+
+            kafkaStream.timeWindowAll(Time.seconds(1)).trigger(new CountTriggerWithTimeout<>(10, TimeCharacteristic.ProcessingTime)).apply(new AllWindowFunction<JSONObject, List<JSONObject>, TimeWindow>() {
+                @Override
+                public void apply(TimeWindow window, Iterable<JSONObject> values, Collector<List<JSONObject>> out) throws Exception {
+                    ArrayList<JSONObject> list = Lists.newArrayList(values);
+                    if (list.size() > 0) {
+                        out.collect(list);
+                    }
+                }
+            }).addSink(new ElasticsearchSink());
+
+            env.execute("kafka2es");
+        } catch (Exception e) {
+            logger.error("kafka2es fail " + e.getMessage());
+        }
     }
 }
